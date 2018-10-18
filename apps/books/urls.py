5600c77e8797c0a65e235e508292394555f650f2
Zach Ashworth <zashworth2@gmail.com>
2018-10-17 22:09:55 -0500

    lacking html css

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -2,4 +2,9 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^add$', views.addbook),
+    url(r'^new$', views.newbook),
+    url(r'^(?P<bookid>\d+)$', views.show),
+    url(r'^addreview$', views.addreview),
+
 ]  
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,32 +2,40 @@ from django.shortcuts import render,redirect
 from .models import *
 from ..books.models import *
 from django.contrib import messages
-
+#------------------------------------------------------------------------
 def index(request):
     return render(request, 'users/index.html')
 def add(request):
     # pass the post data to the method we wrote and save the response in a variable called errors
     errors = User.objects.basic_validator(request.POST)
         # check if the errors object has anything in it
+
     if len(errors):
         # if the errors object contains anything, loop through each key-value pair and make a flash message
         for key, value in errors.items():
             messages.error(request, value)
         # redirect the user back to the form to fix the errors
         return redirect('/')
+    
     else:
         # if the errors object is empty, that means there were no errors!
         # retrieve the user to be updated, make the changes, and save
-        request.session['email']=request.POST['email']
         user = User.objects.create()
+       
+        user.userlevel=0
         user.firstname = request.POST['firstname']
         user.lastname = request.POST['lastname']
         user.email = request.POST['email']
         user.pwhash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
         user.save()
+
+        request.session['userlevel']=user.userlevel
+        request.session['email']=request.POST['email']
+
         messages.success(request, "User successfully added")
         # redirect to a success route
         return redirect('/books')
+#------------------------------------------------------------------------
 def login(request):
     errors = User.objects.login_validator(request.POST)
     if len(errors):
@@ -36,5 +44,11 @@ def login(request):
             messages.error(request, value)
         # redirect the user back to the form to fix the errors
         return redirect('/')
+
+    user=User.objects.get(email=request.POST['email'])
+
+    request.session['userlevel']=user.userlevel
     request.session['email']=request.POST['email']
+
     return redirect('/books')
+#------------------------------------------------------------------------
